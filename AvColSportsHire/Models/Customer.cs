b9041ddc5ddd3deac4a39b93cef76f1c27455314
Giriraj Kumar <ac148764@avcol.school.nz>
2026-03-16 14:59:41 +1300

    started Customer model

---
+++ AvColSportsHire/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using AvColSportsHire.Areas.Identity.Data;
+
+namespace AvColSportsHire.Models
+{
+    public class Customer : SportsHireUser
+    {
+        public int CustomerId { get; set; }
+        public int OrganizationId { get; set; }
+        public Organization Organization { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Phone { get; set; }
+
+    }
+}
